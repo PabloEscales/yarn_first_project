c70bf349b27c568e8f0723a03a8024fb8dcc09f1
Pablo Escales <pabloescales@gmail.com>
2023-01-14 15:12:58 +0100

    testing hooks

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Welcome from "./Welcome.jsx";
+
+const App = () => {
+  return(
+    <div className="App">
+      <Welcome name="Biyo" direcction="Av. Siempre viva 123" />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Welcome.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+export default function Welcome (props) {
+  const name = props.name;
+  const direcction = props.direcction;
+
+  const [state, setState] = useState(false);
+  const [lastName, setLastName] = useState("false");
+
+  const handleClick = () => {
+    setState(!state)
+  }
+
+  const changeLastName = () => {
+    setLastName("Messi")
+  }
+
+  useEffect(() => {
+    console.log("HACIENDO EFECTO")
+  }, [lastName])
+
+  return (
+
+    <div>
+      <h1>Hola { name }</h1>
+      <button onClick={handleClick} > { direcction } </button>
+      <button onClick={changeLastName}> { lastName } </button>
+      <p> { state ? "soy true " : "false" } </p>
+      <div> X </div>
+    </div>
+  );
+}
